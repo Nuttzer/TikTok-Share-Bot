fc3d018118473280c92f0300e55ddbf2565e7eb3
VIPER NUTTZER <88751082+Nuttzer@users.noreply.github.com>
2022-04-23 21:28:50 -0500

    Update share_bot.py

--- share_bot.py
+++ share_bot.py
@@ -1,3 +1,5 @@
+import time
+
 try:
     import requests, string, random, os, platform, ctypes, threading
     from colorama import Fore, init
@@ -6,11 +8,11 @@ except ImportError:
 
 init(convert=True)
 
-print( Fore.RED + "warning there are going to be errors but just let it run ")
+print( Fore.RED +                                   "warning there are going to be errors but just let it run ")
 
 time.sleep(3.0)
 
-print( Fore.RED + "recomended 100 shared per second so that there is less errors")
+print( Fore.RED + "recomended 100 shared per second ")
 
 time.sleep(3.0)
 
@@ -36,20 +38,22 @@ if platform.system() == "Windows":
 else:
     clear = "clear"
 
+
 class tiktok:
 
     def __init__(self):
         self.lock = threading.Lock()
         self.shared = 0
         self.errors = 0
-    
+
     def safe_print(self, arg):
         self.lock.acquire()
         print(arg)
         self.lock.release()
 
     def update_title(self):
-        ctypes.windll.kernel32.SetConsoleTitleW(f"TikTok Share Botter | Sent Shares: {self.shared} | Errors: {self.errors} | https://github.com/Nuttzer/TikTok-Share-Bot")
+        ctypes.windll.kernel32.SetConsoleTitleW(
+            f"TikTok Share Botter | Sent Shares: {self.shared} | Errors: {self.errors} | https://github.com/Nuttzer/TikTok-Share-Bot")
 
     def send_share(self, video_id):
         try:
@@ -58,12 +62,12 @@ class tiktok:
                 "content-type": "application/x-www-form-urlencoded; charset=UTF-8",
                 "user-agent": "com.zhiliaoapp.musically/2022309050 (Linux; U; Android 7.1.2; en; G011A; Build/N2G48H;tt-ok/3.10.0.2)"
             }
-            device_id = ("").join(random.choices(string.digits, k = 19))
+            device_id = ("").join(random.choices(string.digits, k=19))
             session = requests.Session()
             url = f"https://api31-core-useast1a.tiktokv.com/aweme/v1/aweme/stats/?channel=googleplay&device_type=G011A&device_id={device_id}&os_version=7.1.2&version_code=220400&app_name=musically_go&device_platform=android&aid=1988"
-            r = session.post(url,headers=headers,data=data)
+            r = session.post(url, headers=headers, data=data)
             if r.json()["status_code"] == 0:
-                #self.safe_print(f"       {Fore.WHITE}[{Fore.GREEN}Success{Fore.WHITE}] Sent share!")
+                # self.safe_print(f"       {Fore.WHITE}[{Fore.GREEN}Success{Fore.WHITE}] Sent share!")
                 self.shared += 1
             else:
                 self.errors += 1
@@ -71,7 +75,7 @@ class tiktok:
             self.errors += 1
         if clear == "cls":
             self.update_title()
-        
+
     def parse_url(self, url):
         if "vm.tiktok.com" in url:
             r = requests.get(url, allow_redirects=False)
@@ -82,7 +86,7 @@ class tiktok:
         except IndexError:
             pass
         return url
-        
+
     def main(self):
         os.system(clear)
         if clear == "cls":
@@ -93,14 +97,20 @@ class tiktok:
         video_id = self.parse_url(tiktok_url)
         print(f"\n       {Fore.WHITE}[{Fore.RED}Console{Fore.WHITE}] Sending shares...")
         print()
+        time.sleep(2.4)
+
+        print(Fore.LIGHTGREEN_EX + "it will show that there is alot of errors but just let it run | feel free to run a few  at a time  ")
+
         def thread_starter():
             self.send_share(video_id)
+
         while True:
             if threading.active_count() <= threads:
                 try:
-                    threading.Thread(target = thread_starter).start()
+                    threading.Thread(target=thread_starter).start()
                 except:
                     pass
+
 
 obj = tiktok()
 obj.main()
